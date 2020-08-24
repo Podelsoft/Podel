a2a50b8b9995adf159cd75daa5f6b5a99e7dee1c
root <root@instance-1.europe-west2-c.c.boreal-charter-249310.internal>
2020-08-24 16:45:04 +0000

    updated changelog.js and package.json (sorry I'm retarded)

--- commands/changelog.js
+++ commands/changelog.js
@@ -7,15 +7,14 @@ module.exports.run = async (bot, message, args) => {
   let podelemoji = bot.emojis.find(emoji => emoji.name === `podel`);
   
   const embed = new Discord.RichEmbed()
-  .setTitle('v1.0 Changelog | 20/8/2020')
+  .setTitle('v1.1.0 Changelog | 24/8/2020')
   .setDescription(`\`\`\`diff
-+ [update] Updated p!buy (3)
-+ [update] Updated p!sell (3)
-+ [update] Updated p!shop (2)
-+ [update] Updated p!stats (almost entirely)
-+ [update] Updated p!work (to work with all cars)
-+ [command] Added p!setcar (p!setcar <car name>)
-+ [shop] Added the following cars: Chevrolet Cobalt SS & Mazda MX-5 Miata\`\`\``)
++ All previous updates.
++ Added Search By ID for all Moderation commands.
++ Added 3 New Tiers (Tier 1: p!drive and x2 p!work money, Tier 2: all previous perks + x2 p!drive money, Tier 3: all previous perks + x4 p!drive money).
++ New cars: the 2009 Hyundai Sonata, the 2009 Hyundai Accent, the 2007 Kia Rondo EX Wagon, the 2008 Suzuki SX4 Wagon and the 2009 Mitsubishi Galant.
+--- Fixed all missing car release dates at the p!shop.
++ New command: p!msgwarn <message id> <reason> (the 2 other commands p!additem and p!addcar are very experimental and might not work as intended)\`\`\``)
   .setColor(colour)
   .setFooter('Podel, are ya coding son?', bot.user.avatarURL);
   
