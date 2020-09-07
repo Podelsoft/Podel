6c311d65f1a349f0e91bc1e90a204f35ac170788
root <root@koke.zap-hosting.com>
2020-09-07 20:58:43 +0000

    forgot to remove p!clear from the help command lol

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, help) => {
   
   const staffembed = new Discord.MessageEmbed()
   .setTitle(message.author.tag + ' | Commands List')
-  .setDescription('`bazinga`, `podelvid`, `xp`, `changelog`, `info`, `website`, `yep`, `conspiracy`, `yt`, `suggest`, `boiler`, `server`, `podel`, `brick`, `user`, `simp`, `global`, `leaderboard`, `opts`, `apilist`, `kojima`, `daily`, `joblist`, `stats`, `store`, `buy`, `sell`, `lord`, `work`, `drive`, `invite`, `car`, `overlord`, `setcar`, `play`, `add`, `leave`, `skip`, `queue`, `clearqueue`, `volume`, `nowplaying`, `resume`, `pause`, `repeat`, `warn`, `clearwarn`, `kick`, `ban`, `check`, `clear`, `mute`, `unmute`, `tempban`, `unban`, `msgwarn`, `mutemsg`')
+  .setDescription('`bazinga`, `podelvid`, `xp`, `changelog`, `info`, `website`, `yep`, `conspiracy`, `yt`, `suggest`, `boiler`, `server`, `podel`, `brick`, `user`, `simp`, `global`, `leaderboard`, `opts`, `apilist`, `kojima`, `daily`, `joblist`, `stats`, `store`, `buy`, `sell`, `lord`, `work`, `drive`, `invite`, `car`, `overlord`, `setcar`, `play`, `add`, `leave`, `skip`, `queue`, `clearqueue`, `volume`, `nowplaying`, `resume`, `pause`, `repeat`, `warn`, `clearwarn`, `kick`, `ban`, `check`, `mute`, `unmute`, `tempban`, `unban`, `msgwarn`, `mutemsg`')
   .setColor(colour)
   .setFooter('Podel, coded by the government of georgia', bot.user.avatarURL());
   
