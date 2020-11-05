524ce77fde496108a192f45cdeb5fa4dc5fac3a3
root <root@koke.zap-hosting.com>
2020-11-05 17:37:30 +0000

    deepscan yapping again

--- commands/play.js
+++ commands/play.js
@@ -1,13 +1,16 @@
+/*
 const Discord = require("discord.js");
 const yts = require("yt-search");
 let config = require("../config.json"),
   colour = config.colour;
+*/
 
 module.exports.run = async (bot, message, args) => {
 
   return message.channel.send("temporarily disabled.");
 
-  /*let podelemoji = bot.emojis.cache.find(emoji => emoji.name === `podel`);
+  /*
+  let podelemoji = bot.emojis.cache.find(emoji => emoji.name === `podel`);
 
   if (!args[0]) return;
 
@@ -56,7 +59,8 @@ module.exports.run = async (bot, message, args) => {
     }
   } else {
     message.reply("you need to join a voice channel first.");
-  }*/
+  }
+  */
 };
 
 module.exports.help = {
